4a32ee20fed517bef4c867a4c3731cb89eb3ac33
Saurav Sah <saurav.sah@mavs.uta.edu>
2021-09-10 17:10:10 -0500

    Add

---
+++ simple_todo_app/app/src/main/java/com/example/simple_todo_app/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.simple_todo_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<String> items;
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        etItem = findViewById(R.id.etItem);
+        rvItems = findViewById(R.id.rvitems);
+
+
+
+
+        items = new ArrayList<>();
+        items.add("Buy Milk");
+        items.add("Go to the gym");
+        items.add("Have fun!!");
+
+        ItemsAdapter itemsAdapter=  new ItemsAdapter(items);
+        rvItems.setAdapter(itemsAdapter);
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String todoItem = etItem.getText().toString();
+                //Add item to the model
+                items.add(todoItem);
+                //Notify adapter that an item is inserted
+                itemsAdapter.notifyItemInserted(items.size()-1);
+                etItem.setText("");
+                Toast.makeText(getApplicationContext(), "Item was added", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
